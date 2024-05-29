9cf0929f74f04c2a3e6189f994eb2abcec5ebc72
M. Antonella Tesio <antotesio@gmail.com>
2024-05-29 15:53:38 +1000

    Comienzo de conexion con base de datos

---
+++ Login/src/main/java/com/login/servlet/LoginServlet.java
@@ -0,0 +1,45 @@
+package com.login.servlet;
+
+/**
+ *
+ * @author antot
+ */
+
+import com.login.database.DatabaseConnection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                response.sendRedirect("welcome.html");
+            } else {
+                response.sendRedirect("login.html");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.sendRedirect("error.html");
+        }
+    }
+}
