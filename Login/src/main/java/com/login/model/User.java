506c171a79ec6f5ba50f1683eb30e6322792e38f
M. Antonella Tesio <antotesio@gmail.com>
2024-05-30 12:50:44 +1000

    Comienzo de formulario

--- Login/src/main/java/com/login/model/User.java
+++ Login/src/main/java/com/login/model/User.java
@@ -5,20 +5,10 @@ package com.login.model;
  * @author antot
  */
 public class User {
-    private int id;
     private String username;
-    private String lastname;
-    private int age;
     private String mail;
     private String password;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getUsername() {
         return username;
@@ -26,22 +16,6 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     public String getMail() {
