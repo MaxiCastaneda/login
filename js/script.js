3b83529dce4af72325c35ede1ead602f0227c378
Maxi Castañeda <maxicastaneda@gmail.com>
2024-06-02 12:52:56 +1000

    Border and eye design update 2.6.24

--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('.nav-links');
+    const passwordInput = document.getElementById('password');
+    const togglePasswordButtons = document.querySelectorAll('#togglePassword');
+    const eyeIcon = document.getElementById('eyeIcon');
 
     menuToggle.addEventListener('click', () => {
         navLinks.classList.toggle('active');
     });
 
-    // Mostrar/ocultar contraseña
-    const togglePassword = document.getElementById('togglePassword');
-    const passwordInput = document.getElementById('password');
+    togglePasswordButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
 
-    togglePassword.addEventListener('click', () => {
-        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-        passwordInput.setAttribute('type', type);
-        togglePassword.classList.toggle('active');
-
-        // Alternar entre el icono de ojo abierto y cerrado
-        const icon = togglePassword.querySelector('i');
-        if (icon.classList.contains('fa-eye')) {
-            icon.classList.remove('fa-eye');
-            icon.classList.add('fa-eye-slash');
-        } else {
-            icon.classList.remove('fa-eye-slash');
-            icon.classList.add('fa-eye');
-        }
+            // Alternar entre el icono de ojo abierto y cerrado
+            const icon = button.querySelector('i');
+            if (icon.classList.contains('fa-eye')) {
+                icon.classList.remove('fa-eye');
+                icon.classList.add('fa-eye-slash');
+            } else {
+                icon.classList.remove('fa-eye-slash');
+                icon.classList.add('fa-eye');
+            }
+        });
     });
+
+    function updateEyeIcon() {
+        if (passwordInput === document.activeElement && passwordInput.value.length > 0) {
+            eyeIcon.classList.remove('fa-eye');
+            eyeIcon.classList.add('fa-eye-slash');
+        } else {
+            eyeIcon.classList.remove('fa-eye-slash');
+            eyeIcon.classList.add('fa-eye');
+        }
+    }
+
+    // Listen for input events and focus events on the password input field
+    passwordInput.addEventListener('input', updateEyeIcon);
+    passwordInput.addEventListener('focus', updateEyeIcon);
+    passwordInput.addEventListener('blur', updateEyeIcon);
 });
